6379a3165452e450ec6af88a3d6781a14b495d47
Denphum <asamaporn.jajah@mail.kmutt.ac.th>
2020-10-20 15:12:34 +0700

    FirstCommit

---
+++ src/testgit/TestGit.java
@@ -0,0 +1,13 @@
+
+package testgit;
+
+public class TestGit {
+
+    public static void main(String[] args) {
+        sayHello();
+    }
+    public static void sayHello(){
+        System.out.println("Hello World");
+    }
+    
+}
